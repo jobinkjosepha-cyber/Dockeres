6bba6d12a0befd9370e56c763d2cb5994b9b55db
Jobin Joseph <jobinkjosepa@mail.com>
2025-11-27 10:07:39 +0530

    added

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,7 @@ RUN npm run build
 
 FROM nginx:latest
 COPY --from=build /app/build /usr/share/nginx/html
+
 EXPOSE 80
+
 CMD ["nginx", "-g", "daemon off;"]
--- src/component/Dockerss.jsx
+++ src/component/Dockerss.jsx
@@ -10,6 +10,7 @@ function Dockerss() {
          suscipit magni distinctio illum voluptatibus
           a corporis porro exercitationem dignissimos
            ad cum, soluta quibusdam eos!</p>
+           <h1>hello chunni kutta</h1>
     </div>
   )
 }
